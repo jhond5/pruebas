5c8f3490491c8fcc55c38982d1e824e4520913d9
John Cuervo <johnf_cuervo@hotmail.com>
2018-04-14 00:15:47 -0500

    Modificaciones JCUERVO v2

--- HelloWorld.js
+++ HelloWorld.js
@@ -1,3 +1,8 @@
 var vcVarchar = "Prueba HELLO WORLD";
 var nuNumber = 123;
-var blBoolean = true;
+var blBoolean = true; // Comentarios
+
+var arArray = [1, "Valor 2", true];
+var arArray2 = new Array(1, "Valor 2", false);
+
+console.log("Prueba de" + arArray2[2]);
